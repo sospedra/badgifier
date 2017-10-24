5fbd4522ef6dc1998ef06adac0d7251702557736
sospedra <sospedra.r@gmail.com>
2017-10-24 12:39:37 +0200

    test: Add suite for shield and color

--- test/badgifier.js
+++ test/badgifier.js
@@ -39,3 +39,26 @@ tape('Can guard params', (t) => {
   t.throws(guard.threshold.bind(null, 100, 80), 'should threshold throw range error if lowe is higher')
   t.doesNotThrow(guard.threshold.bind(null, 20, 100), 'should threshold accept a natural range of integers')
 })
+
+tape('Can generate shields', (t) => {
+  const rate = 80
+  const color = 'green'
+
+  t.plan(1)
+
+  t.equals(
+    badgifier.shield(rate, color),
+    `https://img.shields.io/badge/coverage-${rate}-${color}.svg`,
+    'should generate a shield url'
+  )
+})
+
+tape('Can select color', (t) => {
+  const rate = 80
+
+  t.plan(3)
+
+  t.equals(badgifier.color({ tl: rate + 10 }, rate), 'red', 'should be able to select red color')
+  t.equals(badgifier.color({ th: rate + 10 }, rate), 'yellow', 'should be able to select yellow color')
+  t.equals(badgifier.color({}, 100), 'green', 'should be able to select green color')
+})
