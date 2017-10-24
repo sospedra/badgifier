20f6f0a2e70199a90df9c9d9df8b7b3069dffd12
sospedra <sospedra.r@gmail.com>
2017-10-24 12:00:53 +0200

    test: Adjust default suite to guards

--- test/badgifier.js
+++ test/badgifier.js
@@ -11,12 +11,11 @@ const next = () => tests.shift()()
 
 tests.push(() => {
   tape('Can generate default parameters', (t) => {
-    t.plan(5)
+    t.plan(4)
 
     const params = badgifier.parameters()
     const defaultParams = fixtures.params.default
 
-    t.deepLooseEqual(Object.keys(params), Object.keys(defaultParams), 'the params contains all the keys including the alias')
     t.equals(params.input, defaultParams.input, 'the param input is the cobertura-coverage.xml')
     t.equals(params.output, defaultParams.output, 'the param output is console.log')
     t.equals(params['threshold-high'], defaultParams['threshold-high'], 'the param threshold-high is 95')
