1d05a959d93df6e2790ceeb9375ec04994a65d0d
sospedra <sospedra.r@gmail.com>
2017-10-27 21:12:58 +0200

    test: Add insert suite

---
+++ test/fixtures/files.js
@@ -0,0 +1,5 @@
+module.exports = {
+  html: '<h1>Hello</h1>\n<img src="https://img.shields.io/badge/coverage-10-red.svg" alt="badgifier-cobertura" />\n<p>Bye!</p>',
+  markdown: '# Hello\n![badgifier-cobertura](https://img.shields.io/badge/coverage-10-red.svg)\nBye!',
+  mixed: '# Hello\n<img src="https://img.shields.io/badge/coverage-10-red.svg" alt="badgifier-cobertura" />\n<p>Bye!</p>'
+}
